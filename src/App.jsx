1419ac083d6475ece3646226c7202d57206bcc71
paulho1973 <yonghing.ho@gmail.com>
2024-11-27 08:15:15 +0800

    built

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import useSWR from "swr";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [period, setPeriod] = useState(0);
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error, isLoading } = useSWR("https://nextjs-fastapi-henna.vercel.app/api/py/db", fetcher);
@@ -21,6 +22,7 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        /*}
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -28,7 +30,15 @@ function App() {
       </div>
       <h1>Trading1501 Filter Analysis</h1>
       <div className="card">
-        <button>M15</button>
+        <button onClick={() => setPeriod(0)}>
+          M15
+        </button>
+        <button onClick={() => setPeriod(1)}>
+          H4
+        </button>
+        <button onClick={() => setPeriod(2)}>
+          Daily
+        </button>
         {/*
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -49,12 +59,78 @@ function App() {
       </p>
       */}
 
-      { data && data.sort((a, b) => (a.symbol < b.symbol ? 1 : -1)).map((pair, index) => (
+      { isLoading && <h2>Loading...</h2> }
+
+      { (period === 0) && data && data.sort((a, b) => (a.symbol < b.symbol ? 1 : -1)).map((pair, index) => (
         <div>
-          <h2>{index+1}&#41; {pair.symbol}</h2>
+          <h2>{index+1}&#41; {pair.symbol} 
+          {(() => {
+            switch (period) {
+              case 0:
+                return " M15"
+              case 1:
+                return " H4"
+              case 2:
+                return " Daily"
+              default:
+                return null
+            }
+          })()}
+          </h2>
           <img key={index} className="responsive-image" src={"https://server1501.cloud/charts/"+pair.symbol+"M15.png?t="+Date.now()} />
         </div>
       ))}
+
+      { (period === 1) && data && data.sort((a, b) => (a.symbol < b.symbol ? 1 : -1)).map((pair, index) => (
+        <div>
+          <h2>{index+1}&#41; {pair.symbol} 
+          {(() => {
+            switch (period) {
+              case 0:
+                return " M15"
+              case 1:
+                return " H4"
+              case 2:
+                return " Daily"
+              default:
+                return null
+            }
+          })()}
+          </h2>
+          <img key={index} className="responsive-image" src={"https://server1501.cloud/charts/"+pair.symbol+"H4.png?t="+Date.now()} />
+        </div>
+      ))}
+
+      { (period === 2) && data && data.sort((a, b) => (a.symbol < b.symbol ? 1 : -1)).map((pair, index) => (
+        <div>
+          <h2>{index+1}&#41; {pair.symbol} 
+          {(() => {
+            switch (period) {
+              case 0:
+                return " M15"
+              case 1:
+                return " H4"
+              case 2:
+                return " Daily"
+              default:
+                return null
+            }
+          })()}
+          </h2>
+          <img key={index} className="responsive-image" src={"https://server1501.cloud/charts/"+pair.symbol+"D1.png?t="+Date.now()} />
+        </div>
+      ))}
+
+      <p>
+        <span>&#9993;</span> ai@trading1501.com
+      </p>
+
+      <div>
+        <span>This website is built with React.js </span>
+        <img width="25" style={{verticalAlign: "middle"}} src={reactLogo} alt="React logo" />
+      </div>
+
+
 
     </>
   )
