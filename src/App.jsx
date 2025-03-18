e2686ee79e9af4386a442812bd982b948a18d0f7
paulho1973 <yonghing.ho@gmail.com>
2025-03-18 17:36:02 +0800

    change period 2

--- src/App.jsx
+++ src/App.jsx
@@ -267,11 +267,11 @@ function App() {
           {(() => {
             switch (period) {
               case 0:
-                return " M15"
-              case 1:
                 return " H1"
-              case 2:
+              case 1:
                 return " D1"
+              case 2:
+                return " W1"
               default:
                 return null
             }
@@ -288,11 +288,11 @@ function App() {
           {(() => {
             switch (period) {
               case 0:
-                return " M15"
-              case 1:
                 return " H1"
-              case 2:
+              case 1:
                 return " D1"
+              case 2:
+                return " W1"
               default:
                 return null
             }
